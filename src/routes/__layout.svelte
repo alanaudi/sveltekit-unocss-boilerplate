f932e28aa5e11c757bdea37947060ab24784d33c
alanaudi <alanaudi.tw@gmail.com>
2022-06-01 21:30:31 +0800

    chore: add css reset

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,5 +1,15 @@
 <script lang="ts">
   /**
+   * CSS Reset / Normalization
+   */
+  // normalize.css
+  // import '@unocss/reset/normalize.css'
+  // reset.css by Eric Meyer https://meyerweb.com/eric/tools/css/reset/index.html
+  // import '@unocss/reset/eric-meyer.css'
+  // preflights from tailwind
+  import '@unocss/reset/tailwind.css'
+
+  /**
    * styling
    */
   import 'uno.css'
